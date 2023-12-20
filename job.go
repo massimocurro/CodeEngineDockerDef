cf23c8956322d57754877c87dbf4b62eefa23366
Massimo Curro <m.curro@gruppostratos.com>
2023-12-20 17:59:41 +0100

    FIX

--- job.go
+++ job.go
@@ -12,5 +12,5 @@ var target = os.Getenv("Key")
 func main() {
         
 	fmt.Printf("Hi from a batch job! My index within the array of %s instance(s) is %s\n", os.Getenv("JOB_ARRAY_SIZE"), os.Getenv("JOB_INDEX"))
-        http.Get("https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud")
+        http.Get("https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud?key=" +target)
 }
