71f3016add035cf9ecae66695eb0a777f294c5a2
Massimo Curro <m.curro@gruppostratos.com>
2023-12-20 17:25:00 +0100

    FIX

--- job.go
+++ job.go
@@ -4,11 +4,11 @@ package main
 import (
 	"fmt"
 	"os"
-    "net/http"
+        "net/http"
 )
 
 func main() {
-
-	    fmt.Printf("Hi from a batch job! My index within the array of %s instance(s) is %s\n", os.Getenv("JOB_ARRAY_SIZE"), os.Getenv("JOB_INDEX"))
+        targetFile := os.Getenv("Key")
+	fmt.Printf("Hi from a batch job! My index within the array of %s instance(s) is %s\n", os.Getenv("JOB_ARRAY_SIZE"), os.Getenv("JOB_INDEX"))
         http.Get("https://function-76.1at6rgz00yjr.eu-de.codeengine.appdomain.cloud")
 }
